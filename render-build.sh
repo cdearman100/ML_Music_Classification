7f8979011bed5ed4ae9ff17339da350f4f12c45f
CJ <cdearm2@uic.edu>
2025-01-29 15:49:12 -0600

    Fix LLVM installation for llvmlite on Render

--- render-build.sh
+++ render-build.sh
@@ -6,9 +6,8 @@ apt-get update
 # Install LLVM (required for llvmlite and numba)
 apt-get install -y llvm llvm-dev clang
 
-# Verify installation
-llvm-config --version || echo "LLVM installation failed"
+# Install llvmlite from a pre-built wheel
+pip install --find-links https://anaconda.org/numba/llvmlite/files llvmlite
 
 # Continue with normal deployment
-pip install --upgrade pip
 pip install -r requirements.txt
